39426b1995a2ae88a494a68a08a6a3f65eddf9dc
shwetank00001 <mshwetank@gmail.com>
2023-10-03 18:27:25 +0530

    added prod details

--- src/components/Product/ProductDetails.js
+++ src/components/Product/ProductDetails.js
@@ -1,10 +1,35 @@
 import React, { Fragment } from 'react'
 import Carousel from 'react-material-ui-carousel'
+import { useSelector, useDispatch } from 'react-redux'
+import { getProductDetails } from '../../actions/productAction'
+import { match } from 'assert'
+ 
+const ProductDetails = (match) => {
 
-const ProductDetails = () => {
+    const dispatch = useDispatch()
+
+    const {product ,loading, error }  = useSelector(state => state.productDetails)
+
+    React.useEffect( () => {
+        dispatch(getProductDetails(match.params.id))
+    }, [dispatch, match.params.id] )
   return (
     <Fragment>
         <div className='ProductDetails'>
+            <div>
+                <Carousel>
+                    {product.images && 
+                        product.images.map( (item, index) => (
+                            <img 
+                            className='CarouselImage'
+                            key={item.url}
+                            src={item.url}
+                            alt={`${index} Slide`}
+                              />
+                        ))
+                    }
+                </Carousel>
+            </div>
 
         </div>
     </Fragment>
