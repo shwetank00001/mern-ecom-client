98dc2f101908d692979e6cf4b71a1f9b8b7254cc
shwetank00001 <mshwetank@gmail.com>
2023-10-07 14:01:07 +0530

    break

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import Header from './components/layout/Header/Header';
 import WebFont from 'webfontloader';
 import Footer from './components/layout/Footer/Footer';
 import { Routes, Route } from 'react-router-dom';
+
+
 import ProductDetails from './components/Product/ProductDetails.js';
 import Products from './components/Product/Products';
 import Home from './components/Home/Home';
 import Search from './components/Product/Search';
+import LoginSignUp from './components/User/LoginSignUp'
+
 
 const App = () => {
     React.useEffect(() => {
@@ -26,6 +30,7 @@ const App = () => {
                 <Route exact path='/products' element={<Products />} />
                 <Route path='/products/:keyword?' element={<Products />} />
                 <Route exact path='/search' element={<Search />} />
+                <Route exact path='/login' element={<LoginSignUp />} />
             </Routes>
             <Footer />
         </div>
--- src/components/User/LoginSignUp.js
+++ src/components/User/LoginSignUp.js
@@ -1,11 +1,42 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useRef } from 'react';
 import './LoginSignUp.css'
 import Loader from '../layout/loader/Loader';
-
+import { Link } from 'react-router-dom';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import LockOpenIcon from '@mui/icons-material/LockOpen';
 const LoginSignUp = () => {
+
+    const loginTab = useRef(null)
+    const registerTab = useRef(null)
+    const switcherTab = useRef(null)
+
+    const [loginEmail, setLoginEmail] = React.useState("")
+    const [loginPassword, setLoginPassword] = React.useState("")
+
+    const switchTabs = (e, tab) => {
+        if( tab === "login"){
+            switcherTab.current.classList.add("shiftToNeutral")
+            switcherTab.current.classList.remove("shiftToRight")
+
+            registerTab.current.classList.remove("shiftToNeutralForm")
+            loginTab.current.classList.remove("shiftToLeft")
+        }
+        if(tab === "register"){
+            switcherTab.current.classList.add("shiftToRight")
+            switcherTab.current.classList.remove("shiftToNeutral")
+
+            registerTab.current.classList.remove("shiftToNeutralForm")
+            loginTab.current.classList.remove("shiftToLeft")
+        }
+    }
+
+    const loginSubmit = () => {
+        console.log("Login")
+    }
+
   return (
     <Fragment>
-        <div className='LoginSignUpConatainer'>
+        <div className='LoginSignUpContainer'>
             <div className='LoginSignUpBox'>
                 <div>
                     <div className='login-signUp_toggle'>
@@ -15,19 +46,31 @@ const LoginSignUp = () => {
                     <button ref={switcherTab}></button>
                 </div>
 
-                <form className='loginForm' ref={logintab} onSubmit={loginSubmit}>
-                    <MailOutlineIcon />
-                    <input 
-                        type='email'
-                        placeholder='Email'
-                        required
-                        value={loginEmail}
-                        onChange={(e) => setLoginEmail(e.target.value)}
-                    />
-                    <div>
+                <form className='loginForm' ref={loginTab} onSubmit={loginSubmit}>
 
+                    <div className='loginEmail'>
+                        <MailOutlineIcon />
+                        <input 
+                            type='email'
+                            placeholder='Email'
+                            required
+                            value={loginEmail}
+                            onChange={(e) => setLoginEmail(e.target.value)}
+                        />
                     </div>
 
+                    <div className='loginPassword'>
+                        <LockOpenIcon />
+                        <input
+                            type='password'
+                            placeholder='Password'
+                            required
+                            value={loginPassword}
+                            onChange={(e) => setLoginPassword(e.target.value)}
+                            />
+                    </div>
+                    <Link to="/password/forget">Forgot Password ?</Link>
+                    <input type='submit' value="Login" className='loginBtn' />
                 </form>
 
             </div>
